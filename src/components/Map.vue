c692b9b466e50f61fc49de9cc9900a8d5bd2ca0f
Ivan Blohin <ivan.blohinn@yandex.ru>
2019-10-19 13:08:16 +0300

    Отображение точек на карте, обработка события на клик.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,9 +5,41 @@
 <script>
 export default {
   name: "Map",
-  created() {
+  data() {
+    return {
+      leaflet: null,
+      map: null,
+      markersLayerGroup: null,
+      data: [],
+      markers: []
+    };
+  },
+  methods: {
+    fetchData() {
+      this.data = [
+        {
+          lat: 55.796289,
+          lng: 49.108795,
+          meta: 1111
+        }
+      ];
+    },
+    insertMarkers() {
+      this.data.forEach(house => {
+        let marker = this.leafet
+          .marker([house.lat, house.lng])
+          .on("click", e => console.log(e));
+        marker.meta = house;
+        marker.addTo(this.markersLayerGroup);
+      });
+    }
+  },
+  beforeCreate() {
     // eslint-disable-next-line no-undef
     this.leafet = L;
+  },
+  created() {
+    this.fetchData();
   },
   mounted() {
     this.map = this.leafet
@@ -16,11 +48,15 @@ export default {
         minZoom: 6,
         zoomControl: false
       })
-      .setView([54.318463, 48.397007], 13); // center, zoom
+      .setView([55.796289, 49.108795], 12); // center, zoom
 
     this.leafet
       .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
       .addTo(this.map);
+
+    this.markersLayerGroup = this.leafet.layerGroup().addTo(this.map);
+
+    this.insertMarkers();
   }
 };
 </script>
