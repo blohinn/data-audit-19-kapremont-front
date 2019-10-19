0f31a1552e4c72e79676e943613f4e28c08bd74c
Ivan Blohin <ivan.blohinn@yandex.ru>
2019-10-19 12:02:07 +0300

    Компонент карты

---
+++ src/components/Map.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="map" class="map"></div>
+</template>
+
+<script>
+export default {
+  name: "Map",
+  created() {
+    // eslint-disable-next-line no-undef
+    this.leafet = L;
+  },
+  mounted() {
+    this.map = this.leafet
+      .map("map", {
+        preferCanvas: true,
+        minZoom: 6,
+        zoomControl: false
+      })
+      .setView([54.318463, 48.397007], 13); // center, zoom
+
+    this.leafet
+      .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+      .addTo(this.map);
+  }
+};
+</script>
+
+<style scoped>
+.map {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/views/MapView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="map-view">
+    <Map></Map>
+  </div>
+</template>
+
+<script>
+import Map from "@/components/Map.vue";
+
+export default {
+  name: "MapView",
+  components: {
+    Map
+  }
+};
+</script>
+
+<style scoped>
+.map-view {
+  height: 100%;
+}
+</style>
